47b392e7d3cdb5e2eac432836aed92ede04bb3ed
cozydenis <djaferi.denis@gmail.com>
2026-04-05 12:02:30 +0200

    edit logo

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -57,9 +57,9 @@ export function Header() {
             <Image
               src="/imgs/logo.jpeg"
               alt="Sanfora Timepiece"
-              width={120}
-              height={40}
-              className="h-10 w-auto object-contain"
+              width={168}
+              height={56}
+              className="h-14 w-auto object-contain"
               priority
             />
           </Link>
